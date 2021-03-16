e281069d5b17e104e168d4fef8d57c8c24c692c5
hans <hanhshuai@163.com>
2021-03-17 00:39:41 +0800

    修改依赖

--- cloud-api-common/src/main/java/coom/hans/cloud/common/entities/CommonResult.java
+++ cloud-api-common/src/main/java/coom/hans/cloud/common/entities/CommonResult.java
@@ -1,12 +1,16 @@
 package coom.hans.cloud.common.entities;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @Author: hans
  * @Date: 2021/3/14 17:55
  */
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CommonResult {
     private Integer statusCode;
     private String message;
